60ba334d05e9e394d1c582fa5ea8cd8f7b6978f3
luciano <lucianoadfilho@gmail.com>
2018-05-03 12:35:53 -0300

    added new classes

--- BoggleLibrary/src/shared/model/User.java
+++ BoggleLibrary/src/shared/model/User.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.ecomp.uefs.model;
+package shared.model;
 
 import java.io.Serializable;
 
@@ -12,5 +12,19 @@ import java.io.Serializable;
  * @author luciano
  */
 public class User implements Serializable{
-    
+    String username;
+    String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }        
 }
--- BoggleLibrary/src/shared/util/ClientServer.java
+++ BoggleLibrary/src/shared/util/ClientServer.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 
+package shared.util;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
