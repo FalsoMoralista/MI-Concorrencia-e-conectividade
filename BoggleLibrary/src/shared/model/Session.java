1581c29febabc9bfb13ff40faf5f630aa8e3d7b0
Luciano Filho <lucianoadfilho@gmail.com>
2018-05-04 15:35:19 -0300

    added new modeling and exceptions

---
+++ BoggleLibrary/src/shared/model/Session.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shared.model;
+
+import java.io.Serializable;
+
+/**
+ *  Binds an user to a session.
+ * @author Luciano Araujo Dourado Filho
+ */
+public class Session implements Serializable{
+
+    private User user;
+    
+    private String username;
+    private String password;
+    
+    public Session(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+    
+    public Session(User user) {
+        this.user = user;
+    }
+    
+    /**
+     *  Returns the user binded to this session. 
+     */
+    public User getUser() {
+        return user;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
--- BoggleLibrary/src/shared/model/User.java
+++ BoggleLibrary/src/shared/model/User.java
@@ -27,4 +27,19 @@ public class User implements Serializable{
     public void setUsername(String username) {
         this.username = username;
     }        
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return username;
+    }        
 }
