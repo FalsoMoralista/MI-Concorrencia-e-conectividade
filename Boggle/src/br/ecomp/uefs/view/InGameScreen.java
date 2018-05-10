554f1d399d4973d1ce7dc2ef3209a1ed9cfd253e
luciano <lucianoadfilho@gmail.com>
2018-05-09 23:12:41 -0300

    new stuff

--- Boggle/src/br/ecomp/uefs/view/InGameScreen.java
+++ Boggle/src/br/ecomp/uefs/view/InGameScreen.java
@@ -10,10 +10,13 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventType;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
@@ -26,11 +29,16 @@ import shared.model.GameDices;
  */
 public class InGameScreen extends Application {
 
-    private Button[][] buttons;
+    private Button[][] buttons = new Button[4][4];
+    private TextField textField = new TextField();
+    private Button submit = new Button("submit");
+    private Button clear = new Button("clear");
+    private Button x = new Button("x");
+    private LinkedList<String> myWords = new LinkedList<>();
+    private LinkedList<String> letterSet = new LinkedList<>();
 
     public InGameScreen() {
         dices = new GameDices();
-        buttons = new Button[4][4];
     }
 
     private GameDices dices;
@@ -45,15 +53,19 @@ public class InGameScreen extends Application {
         grid.setHgap(0);
         grid.setVgap(15);
         grid.autosize();
+        grid.add(textField, 0, 1);
+        grid.add(submit, 1, 2);
+        grid.add(clear, 2, 2);
+        grid.add(x, 0, 2);
         grid.setPadding(new Insets(20, 20, 20, 20));
         Scene scene = new Scene(grid, 800, 600);
         primaryStage.setScene(scene);
     }
 
     /**
-     * Initialize all buttons.
+     * Initialize all dices.
      */
-    public void initializeButtons() {
+    public void initializeDices() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 buttons[i][j] = new Button();
@@ -61,13 +73,65 @@ public class InGameScreen extends Application {
         }
     }
 
+    /**
+     * Set all button actions.
+     */
+    private void setActions() {
+        LinkedList<String> list = new LinkedList<>();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                String btnName = buttons[i][j].getText();
+                buttons[i][j].setOnAction(e -> {
+                    if (letterSet.contains(btnName)) {
+                        letterSet.remove(btnName);
+                        textField.setText(textField.getText() + btnName);
+                    } else {
+                        System.out.println("throw no letters remaining exception, (all letters available added)");
+                    }
+                });
+            }
+        }
+        clear.setOnAction(e -> textField.clear());
+        submit.setOnAction(e -> {
+            String txt = textField.getText();
+            if (!myWords.contains(txt)) {
+                myWords.add(txt);
+                int j= 1;
+                for(int i = 0; i < txt.length(); i++){
+                    letterSet.add(txt.substring(i, j++));
+                }
+                textField.clear();
+            }else{
+                System.out.println("throw word already added exception");
+            }
+        });
+
+        x.setOnAction(e -> {
+            String text = textField.getText();
+            letterSet.add(text.substring(text.length() - 1));
+            textField.setText(textField.getText().substring(0, textField.getText().length() - 1));
+        });
+        textField.setEditable(false);
+    }
+
+    /**
+     * Reset all configurations.
+     */
+    private void reset() {
+        this.shuffleDices();
+        this.randomizeDices();
+        this.myWords.clear();
+        this.letterSet.clear();
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         this.setUp(primaryStage);
-        this.initializeButtons();
+        this.initializeDices();
         this.shuffleDices();
         this.randomizeDices();
+        this.setActions();
         GridPane grid = (GridPane) primaryStage.getScene().getRoot();
 
         TilePane pane = new TilePane();
@@ -81,7 +145,7 @@ public class InGameScreen extends Application {
                 pane.getChildren().add(buttons[i][j]);
             }
         }
-        
+
         grid.add(pane, 0, 0);
         primaryStage.show();
     }
@@ -107,7 +171,7 @@ public class InGameScreen extends Application {
     }
 
     /**
-     * Generate the random letters for the dices.
+     * Generate the random letters for the dices and add them to a set.
      */
     public void randomizeDices() {
         int minimum = 0;
@@ -121,10 +185,10 @@ public class InGameScreen extends Application {
             int column = Math.abs(minimum + randomNum);
             randomized[row] = dices.DICES[row][column];
         }
-
         int get = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
+                letterSet.add(randomized[get]);
                 buttons[i][j].setText(randomized[get++]);
             }
         }
