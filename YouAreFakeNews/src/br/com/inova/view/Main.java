b2ec449235acf0080521c5426224a47c2be308e3
luciano <lucianoadfilho@gmail.com>
2018-07-19 19:57:26 -0300

    updates

--- YouAreFakeNews/src/br/com/inova/view/Main.java
+++ YouAreFakeNews/src/br/com/inova/view/Main.java
@@ -5,10 +5,15 @@
  */
 package br.com.inova.view;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Properties;
 import java.util.Scanner;
 
@@ -27,27 +32,32 @@ public class Main {
     }
 
     /**
-     *  Show the menu options. 
+     * Show the menu options.
+     * @throws java.io.IOException
      */
-    public void menu() {
+    public void menu() throws IOException {
         scanner = new Scanner(System.in);
-        System.out.println("Menu");
-        System.out.println("------------------------------");
-        System.out.println("| 1 - Rate a news | 0 - exit |");
-        System.out.println("------------------------------");
-        switch (Integer.parseInt(scanner.nextLine())) {
-            case 1:
-                System.out.println("Choose one of the options below:");
-                listNews();
-                int newsID = Integer.parseInt(scanner.nextLine());
-                System.out.println("inform the rating:");
-                int rate = Integer.parseInt(scanner.nextLine());
-                rateNews(newsID, rate);
-                break;
-            case 0:
-                System.exit(0);
-                break;
+        boolean op = true;
+        while (op) {
+            System.out.println("Menu");
+            System.out.println("------------------------------");
+            System.out.println("| 1 - Rate a news | 0 - exit |");
+            System.out.println("------------------------------");
+            switch (Integer.parseInt(scanner.nextLine())) {
+                case 1:
+                    System.out.println("Choose one of the options below:");
+                    listNews();
+                    int newsID = Integer.parseInt(scanner.nextLine());
+                    System.out.println("inform the rating:");
+                    int rate = Integer.parseInt(scanner.nextLine());
+                    rateNews(newsID, rate);
+                    break;
+                case 0:
+                    op = false;
+                    break;
+            }
         }
+        System.exit(0);
     }
 
     /**
@@ -66,9 +76,26 @@ public class Main {
      *
      * @param newsID
      * @param rate
+     * @throws java.io.IOException
      */
-    public static void rateNews(int newsID, int rate) {
+    public void rateNews(int newsID, int rate) throws IOException {
         System.out.println("Rating " + rate + " to the news " + newsID);
+
+        String name = NEWS_LIST.getProperty("NEWS_NAME" + '[' + Integer.toString(newsID) + ']');
+
+        File db = new File("db/news/" + name + ".txt");
+
+        if (!db.exists()) {
+            db.createNewFile();
+        }
+
+        Path path = Paths.get(db.getPath());
+
+        try (BufferedWriter writer = Files.newBufferedWriter(path, StandardOpenOption.APPEND)) {
+            writer.write(String.valueOf(rate));
+            writer.newLine();
+        }
+        System.out.println("Sucessfully");
     }
 
     public static void main(String[] args) throws IOException {
---
+++ YouAreFakeNews/src/interfaces/Services.java
@@ -0,0 +1,23 @@
+package interfaces;
+
+import java.io.IOException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Properties;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Defines the remote methods.
+ *
+ * @author Luciano Araujo Dourado Filho <ladfilho@gmail.com>
+ */
+public interface Services extends Remote {
+
+    public void rateNews(int newsID, int rate) throws IOException, RemoteException;
+    public int getTrunkAVG(int newsID)throws RemoteException;
+    public Properties getNews();
+}
